976d1c1efabb6db2cf1d3214f43ab5d3f82e1ffb
joseluis gomez <joseluisgomezcecegna@gmail.com>
2024-06-03 14:37:17 -0700

    Confugured for server deployment.

--- application/config/config.php
+++ application/config/config.php
@@ -26,8 +26,8 @@ date_default_timezone_set('America/Tijuana');
 | a PHP script and you can easily do that on your own.
 |
 */
-#$config['base_url'] = 'http://localhost/order-manager/';
-$config['base_url'] = 'http://localhost/leanquattro/';
+$config['base_url'] = 'http://avanti-fs/leanquattro/';
+#$config['base_url'] = 'http://localhost/leanquattro/';
 
 /*
 |--------------------------------------------------------------------------
