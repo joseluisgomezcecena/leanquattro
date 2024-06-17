046b699d75622df3e011a087888b3eccb576d30e
joseluis gomez <joseluisgomezcecegna@gmail.com>
2024-06-17 14:29:36 -0700

    changed base url.

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ date_default_timezone_set('America/Tijuana');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.165.1.65/leanquattro/';
+$config['base_url'] = 'http://192.168.1.65/leanquattro/';
 //$config['base_url'] = 'http://localhost/leanquattro/';
 
 /*
