4aa1ba2cf1a285483a6f2c387d74a1f0a3fd2bc9
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2024-10-04 13:36:25 -0700

    Added real made part for client.

--- application/views/hourbyhour/client_update copy.php
+++ application/views/hourbyhour/client_update copy.php
@@ -4,7 +4,7 @@
         <nav class="breadcrumb breadcrumb-dash">
             <a href="<?php echo base_url(); ?>" class="breadcrumb-item"><i class="anticon anticon-home m-r-5"></i>Inicio</a>
             <a class="breadcrumb-item" href="<?php echo base_url("parts") ?>">Hora por Hora</a>
-            <span class="breadcrumb-item active">Capturar Orden De Trabajo</span>
+            <span class="breadcrumb-item active">Actualizar Orden De Trabajo</span>
         </nav>
     </div>
 </div>
@@ -56,7 +56,7 @@
 
                             <div class="form-group col-lg-12 m-b-15">
                                 <label for="workorder_number">Estación de trabajo</label>
-                                <select class="select2" name="wo_workstation" disabled>
+                                <select class="select2" name="wo_workstation" required>
                                     <option value="">Seleccionar Estación de trabajo</option>
                                     <?php foreach($workstations as $workstation) { ?>
                                        
@@ -71,9 +71,9 @@
                            
 
                             <div class="form-group col-lg-12">
-                                <label for="start_date">Fecha de planeacion </label>
+                                <label for="start_date">Fecha de inicio </label>
                                 <!-- echo the selected start date in dd/mm/aaaa format -->
-                                <input type="date" class="form-control" id="start_date" name="start_date" value="<?php echo date('Y-m-d', strtotime($work_order['start_date'])); ?>" readonly>
+                                <input type="date" class="form-control" id="start_date" name="start_date" value="<?php echo date('Y-m-d', strtotime($work_order['start_date'])); ?>">
 
 
                                 <?php echo form_error('start_date', '<div class="text-danger">', '</div>'); ?>
@@ -82,7 +82,7 @@
 
                             <div class="form-group col-lg-12" >
                                 <label for="part_description">Notas de la orden</label>
-                                <textarea type="text" class="form-control" id="notes" name="notes" rows="5" placeholder="Descripción" disabled><?php echo $work_order['notes'] ?></textarea>
+                                <textarea type="text" class="form-control" id="notes" name="notes" rows="5" placeholder="Descripción"><?php echo $work_order['notes'] ?></textarea>
                                 <?php echo form_error('notes', '<div class="text-danger">', '</div>'); ?>
                             </div>
 
@@ -112,36 +112,18 @@
                                     <td>Hora</td>
                                     <td>Numero de parte</td>
                                     <td>Cantidad Planeada</td>
-                                    <td>Parte Realizada</td>
                                     <td>Cantidad Realizada</td>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php 
                                 $hours = 24;
-
-                                $all_part_numbers = [];
-                                for($i = 0; $i < $hours; $i++) {
-                                    $hour = $i < 10 ? "0".$i : $i;
-                                    $part_number = isset($hourbyhour[$hour."p"]) ? $hourbyhour[$hour."p"] : '';
-                                    if ($part_number != '') {
-                                        $all_part_numbers = array_merge($all_part_numbers, explode(' ', $part_number));
-                                    }
-                                }
-
-                                $all_part_numbers = array_filter($all_part_numbers);
-                                //$all_part_numbers = array_unique($all_part_numbers);
-
                                 for($i = 0; $i < $hours; $i++) :
                                     $hour = $i < 10 ? "0".$i : $i;
                                     $part_number = isset($hourbyhour[$hour."p"]) ? $hourbyhour[$hour."p"] : '';
                                     $quantity = isset($hourbyhour[$hour."h"]) ? $hourbyhour[$hour."h"] : '';
                                     $done = isset($hourbyhour[$hour."r"]) ? $hourbyhour[$hour."r"] : '';
                                 ?>
-
-                                <?php echo $all = $part_number; ?>
-
-                                
                                     <tr id="id_<?php echo $hour ?>">
                                         <td><?php echo $hour . ":00 - " . ($hour+1) . ":00"; ?> </td>
                                         
@@ -152,17 +134,6 @@
                                         <td>
                                             <?php echo $quantity != '' ? $quantity : "<b style='color:red'>No planeado</b>"; ?>
                                            
-                                        </td>
-                                        <td>
-                                             <!--parte realizada-->
-                                            <select class="select2" name="part_number_<?php echo $hour ?>" placeholder="Numero de parte">
-                                                <option value="">Seleccionar Numero de parte</option>
-                                                <?php foreach($all_part_numbers as $parte) { ?>
-                                                    <option value="<?php echo $parte; ?>" <?php echo $parte == $part_number ? 'selected' : ''; ?>>
-                                                        <?php echo $parte; ?>
-                                                    </option>
-                                                <?php } ?>
-                                            </select>
                                         </td>
                                         <td>
                                             <input type="number" class="form-control" name="done_<?php echo $hour ?>"  placeholder="Cantidad Realizada" value="<?php echo $done; ?>">
