c77fa46ba54ba052353088837540117515ccf6b3
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2024-11-04 11:30:06 -0800

    Added back button to single andon alert form.

--- application/views/operators/single.php
+++ application/views/operators/single.php
@@ -86,3 +86,11 @@
         </div>
     </div>
 </div>
+
+<div class="fixed bottom-4 right-4">
+    <a href="<?php echo base_url("operator/andon") ?>" class="btn btn-circle btn-green">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+        </svg>
+    </a>
+</div>
